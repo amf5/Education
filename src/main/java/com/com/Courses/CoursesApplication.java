1114477e06679a6cba30e186234d85d31eb57b22
amf5 <ahmedwalidamin648@gmail.com>
2025-08-03 17:30:48 +0300

    add api user

--- src/main/java/com/com/Courses/CoursesApplication.java
+++ src/main/java/com/com/Courses/CoursesApplication.java
@@ -13,6 +13,8 @@ public class CoursesApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(CoursesApplication.class, args);
+		System.out.println("API user test");
+
 	}
 
 }
